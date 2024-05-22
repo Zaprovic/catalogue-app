3798a347debc6f69d339ac331f402f795a4070b0
Zaprovic <jazaro2001@gmail.com>
2024-05-22 06:41:42 -0500

    added local env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   dialect: "sqlite",
   driver: "turso",
   dbCredentials: {
-    url: "libsql://catalogue-app-db-zaprovic.turso.io",
-    authToken:
-      "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTYzMzQ3MDYsImlkIjoiNzgxZjM5NzYtMTUwOC00OWNiLThlNjUtYzFjOTQzMDNhOTY0In0.j3ftQXdnASxsyR0zfVDls53EYHuYD8UcUST9in95V9Yqef2llCJHkoEFa5W3I2n15Xs-tlIOGVa_3p7pVV-JCg",
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 });
--- src/db/main.ts
+++ src/db/main.ts
@@ -3,9 +3,8 @@ import { drizzle } from "drizzle-orm/libsql";
 
 // todo: fix .env issue
 const client = createClient({
-  url: "libsql://catalogue-app-db-zaprovic.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTYzMzQ3MDYsImlkIjoiNzgxZjM5NzYtMTUwOC00OWNiLThlNjUtYzFjOTQzMDNhOTY0In0.j3ftQXdnASxsyR0zfVDls53EYHuYD8UcUST9in95V9Yqef2llCJHkoEFa5W3I2n15Xs-tlIOGVa_3p7pVV-JCg",
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client);

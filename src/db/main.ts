edc0db3f2567c98661048abef2b146b32da3c592
Zaprovic <jazaro2001@gmail.com>
2024-05-22 06:48:03 -0500

    added local env variables

--- src/db/main.ts
+++ src/db/main.ts
@@ -3,8 +3,9 @@ import { drizzle } from "drizzle-orm/libsql";
 
 // todo: fix .env issue
 const client = createClient({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_AUTH_TOKEN!,
+  url: "libsql://catalogue-app-db-zaprovic.turso.io",
+  authToken:
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTYzMzQ3MDYsImlkIjoiNzgxZjM5NzYtMTUwOC00OWNiLThlNjUtYzFjOTQzMDNhOTY0In0.j3ftQXdnASxsyR0zfVDls53EYHuYD8UcUST9in95V9Yqef2llCJHkoEFa5W3I2n15Xs-tlIOGVa_3p7pVV-JCg",
 });
 
 export const db = drizzle(client);

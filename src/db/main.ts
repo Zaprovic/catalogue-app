43e3925cfeb8654369d25737ba94f256ee879a5a
Zaprovic <jazaro2001@gmail.com>
2024-05-22 06:59:12 -0500

    fix:environment variables needed to be loaded ahead of time

--- src/db/main.ts
+++ src/db/main.ts
@@ -1,11 +1,13 @@
 import { createClient } from "@libsql/client";
+import * as dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
 
-// todo: fix .env issue
+dotenv.config();
+
+// fixed: .env needed to be loaded ahead of time
 const client = createClient({
-  url: "libsql://catalogue-app-db-zaprovic.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTYzMzQ3MDYsImlkIjoiNzgxZjM5NzYtMTUwOC00OWNiLThlNjUtYzFjOTQzMDNhOTY0In0.j3ftQXdnASxsyR0zfVDls53EYHuYD8UcUST9in95V9Yqef2llCJHkoEFa5W3I2n15Xs-tlIOGVa_3p7pVV-JCg",
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client);

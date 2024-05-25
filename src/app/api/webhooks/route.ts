ed23e1a76cadfd3b7408af4cdee8eba6c8669250
Zaprovic <jazaro2001@gmail.com>
2024-05-25 11:54:43 -0500

    change webhook secret to work in local env

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -6,7 +6,7 @@ import { Webhook } from "svix";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.LOCAL_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(

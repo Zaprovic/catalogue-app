33d535b141e28cd9e5c26d98e6a102298c1a6834
Zaprovic <jazaro2001@gmail.com>
2024-05-27 18:33:22 -0500

    .

--- src/app/(website)/register/page.tsx
+++ src/app/(website)/register/page.tsx
@@ -12,7 +12,7 @@ const Page = async () => {
   return (
     <main className="flex h-full flex-col">
       <h1 className="text-3xl font-semibold -tracking-wider">
-        Registra tus productos
+        Crea tus productos
       </h1>
 
       <section className="mx-auto my-5 grid w-full max-w-[1000px] grid-cols-1 gap-3">

1832875f7da4c42c99e7911022d71c35fa9b0f13
Zaprovic <jazaro2001@gmail.com>
2024-05-27 13:25:48 -0500

    configured next auth with google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,7 @@
 import { handlers } from "@/auth";
-import NextAuth from "next-auth";
 
 export const { GET, POST } = handlers;
 
-export const {
-  handlers: routeHandlers,
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  providers: [],
-});
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [],
+// });

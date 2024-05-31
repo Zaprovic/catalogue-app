d3064c2635974f4e04c5c2f54c4a5022899bbd83
Zaprovic <jazaro2001@gmail.com>
2024-05-30 20:47:42 -0500

    modified update product form to work with server actions

--- src/app/(website)/dashboard/page.tsx
+++ src/app/(website)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import { revalidateProducts } from "@/actions/revalidate-actions";
 import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
 import { db } from "@/db/main";
 import { ProductTable } from "@/db/schema";
 import styles from "@/styles.module.css";
 import { SelectProductType } from "@/types";
-import { IconRefresh } from "@tabler/icons-react";
 import { eq } from "drizzle-orm";
 import MyProduct from "./(components)/my-product";
 
@@ -13,8 +10,6 @@ export const revalidate = 0;
 
 const Page = async () => {
   const session = await auth();
-
-  // if (!session) redirect("/");
 
   let myProducts: SelectProductType[] = [];
 
@@ -33,13 +28,6 @@ const Page = async () => {
         <h2 className="mb-5 text-2xl font-semibold -tracking-wider">
           Mis productos
         </h2>
-
-        <form action={revalidateProducts} className="my-5">
-          <Button className="flex gap-2">
-            <span>Refresh</span>
-            <IconRefresh />
-          </Button>
-        </form>
 
         <div className={`${styles.myProductContainer} mx-auto max-w-[1200px]`}>
           {myProducts.map((product) => (

fb309be98cacf2ebb9842c21a9af50d3ec908b03
Zaprovic <jazaro2001@gmail.com>
2024-06-06 18:52:25 -0500

    passed product and categories as props to avoid fetching in the client

--- src/app/(website)/dashboard/product/[id]/page.tsx
+++ src/app/(website)/dashboard/product/[id]/page.tsx
@@ -1,18 +1,26 @@
+import ProductCategoryUpdateForm from "@/components/forms/product-category-update-form";
 import ProductUpdateForm from "@/components/forms/products/product-update-form";
 import { db } from "@/db/main";
-import { ProductTable } from "@/db/schema";
+import { CategoryTable, ProductCategoryTable, ProductTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export const revalidate = 0;
 
 const Page = async ({ params }: { params: { id: string } }) => {
-  const product = (
-    await db.select().from(ProductTable).where(eq(ProductTable.id, +params.id))
-  )[0];
+  const singleProduct = await db.query.ProductTable.findFirst({
+    where: eq(ProductTable.id, +params.id),
+  });
+  const categories = await db.select().from(CategoryTable).all();
+  const productCategories = await db.select().from(ProductCategoryTable).all();
+
   return (
     <main className="p-6">
-      <section className="w-full max-w-5xl">
-        <ProductUpdateForm {...product} />
+      <section className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
+        <ProductUpdateForm {...singleProduct} />
+        <ProductCategoryUpdateForm
+          categories={categories}
+          productCategories={productCategories}
+        />
       </section>
     </main>
   );

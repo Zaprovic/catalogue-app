6f34770d4b16e613adc4788100264cacc3a28b42
Zaprovic <jazaro2001@gmail.com>
2024-06-08 12:18:02 -0500

    added filter categories in mobile devices

--- src/components/categories-filter-btn.tsx
+++ src/components/categories-filter-btn.tsx
@@ -39,29 +39,31 @@ const CategoriesFilterBtn = ({
       <h4 className="text-xs font-bold text-primary">{category?.name}</h4>
       <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
         <DropdownMenuTrigger asChild>
-          <Button size={"icon"} variant={"secondary"} className="md:hidden">
+          <Button variant={"secondary"} className="flex gap-2 px-4 md:hidden">
+            <span className="text-sm font-semibold -tracking-wide">
+              Filtrar
+            </span>
             <IconFilter />
           </Button>
         </DropdownMenuTrigger>
 
         <DropdownMenuContent className="w-fit">
           <DropdownMenuLabel>Categorias</DropdownMenuLabel>
-          <DropdownMenuGroup>
+          <DropdownMenuGroup className="flex flex-col items-start gap-0">
             {categories.map((category) => (
-              <DropdownMenuItem key={category.id}>
+              <DropdownMenuItem key={category.id} className="w-full">
                 <Toggle
                   asChild
                   aria-label={`Toggle ${category.name}`}
-                  className="w-full"
+                  className={cn("w-full font-semibold", {
+                    "bg-primary/20 text-primary": pathname.includes(
+                      `/products/categories/${category.id}`,
+                    ),
+                  })}
                 >
                   <Link
                     href={`/products/categories/${category.id}`}
                     onClick={onclick}
-                    className={cn("w-full font-semibold", {
-                      "bg-primary/20 text-primary": pathname.includes(
-                        `/products/categories/${category.id}`,
-                      ),
-                    })}
                   >
                     {category.name}
                   </Link>

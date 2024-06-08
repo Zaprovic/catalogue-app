570dfe15fe0c6caf394e249a13190912b4b92b88
Zaprovic <jazaro2001@gmail.com>
2024-06-08 12:03:24 -0500

    added filter categories in mobile devices

--- src/components/categories-filter-btn.tsx
+++ src/components/categories-filter-btn.tsx
@@ -1,6 +1,11 @@
-import { db } from "@/db/main";
-import { CategoryTable } from "@/db/schema";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { SelectCategoryType } from "@/types";
 import { IconFilter } from "@tabler/icons-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import {
   DropdownMenu,
@@ -12,29 +17,61 @@ import {
 } from "./ui/dropdown-menu";
 import { Toggle } from "./ui/toggle";
 
-const CategoriesFilterBtn = async () => {
-  const categories = await db.select().from(CategoryTable).all();
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size={"icon"} variant={"secondary"} className="md:hidden">
-          <IconFilter />
-        </Button>
-      </DropdownMenuTrigger>
+const CategoriesFilterBtn = ({
+  categories,
+}: {
+  categories: SelectCategoryType[];
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      <DropdownMenuContent className="w-fit">
-        <DropdownMenuLabel>Categorias</DropdownMenuLabel>
-        <DropdownMenuGroup>
-          {categories.map((category) => (
-            <DropdownMenuItem key={category.id}>
-              <Toggle aria-label={`Toggle ${category.name}`} className="w-full">
-                <span>{category.name}</span>
-              </Toggle>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+  const pathname = usePathname();
+
+  const onclick = () => {
+    setIsOpen(false);
+  };
+
+  const category = categories.find((category) => {
+    return pathname.includes(`/products/categories/${category.id}`);
+  });
+
+  return (
+    <div className="flex w-full items-center justify-between">
+      <h4 className="text-xs font-bold text-primary">{category?.name}</h4>
+      <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button size={"icon"} variant={"secondary"} className="md:hidden">
+            <IconFilter />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent className="w-fit">
+          <DropdownMenuLabel>Categorias</DropdownMenuLabel>
+          <DropdownMenuGroup>
+            {categories.map((category) => (
+              <DropdownMenuItem key={category.id}>
+                <Toggle
+                  asChild
+                  aria-label={`Toggle ${category.name}`}
+                  className="w-full"
+                >
+                  <Link
+                    href={`/products/categories/${category.id}`}
+                    onClick={onclick}
+                    className={cn("w-full font-semibold", {
+                      "bg-primary/20 text-primary": pathname.includes(
+                        `/products/categories/${category.id}`,
+                      ),
+                    })}
+                  >
+                    {category.name}
+                  </Link>
+                </Toggle>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 

b44f0f8817ddfd659a2ad000d237e558a13f91e1
Zaprovic <jazaro2001@gmail.com>
2024-06-22 18:23:51 -0500

    fix searchbar

--- src/app/(website)/products/(components)/product-card-list.tsx
+++ src/app/(website)/products/(components)/product-card-list.tsx
@@ -8,9 +8,12 @@ import { SelectProductType } from "@/types";
 const ProductCardList = ({ products }: { products: SelectProductType[] }) => {
   const filteredProducts = useStoreItems((state) => state.filteredProducts);
 
+  const productsToDisplay =
+    filteredProducts.length > 0 ? filteredProducts : products;
+
   return (
     <div className={`${style.productContainer} w-full place-items-center`}>
-      {filteredProducts.map((product) => (
+      {productsToDisplay.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
     </div>
